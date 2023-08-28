194a9dc17e082d329237bee062543e64c9a74946
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-08-28 05:18:37 +0300

    first text in home screen done

--- lib/features/home/presentation/views/widgets/home_screen_body.dart
+++ lib/features/home/presentation/views/widgets/home_screen_body.dart
@@ -5,6 +5,32 @@ class HomeScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ListView(
+        children: [
+          RichText(
+            text: const TextSpan(
+              text: 'You\'ve got ',
+              style: TextStyle(
+                fontSize: 26,
+                color: Color.fromRGBO(6, 69, 83, 1),
+              ),
+              children: [
+                TextSpan(
+                  text: '12',
+                  style: TextStyle(
+                    color: Color.fromRGBO(255, 173, 71, 1),
+                  ),
+                ),
+                TextSpan(
+                  text: ' tasks today!',
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
