9ef6c808de49493616b8640c8f343d90aadd4a8d
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-08-28 04:32:38 +0300

    icons in app bar added

--- lib/features/home/presentation/views/home_screen.dart
+++ lib/features/home/presentation/views/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/core/utils/colors.dart';
 import 'widgets/app_bar_title.dart';
 import 'widgets/home_screen_body.dart';
 import 'widgets/profile_picture_app_bar.dart';
@@ -12,6 +13,22 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         leading: const ProfilePictureAppBar(),
         title: const AppBarTitle(),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              color: MyColors.mainColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.notifications_none_outlined,
+              color: MyColors.mainColor,
+            ),
+          ),
+        ],
       ),
       body: const HomeScreenBody(),
     );
