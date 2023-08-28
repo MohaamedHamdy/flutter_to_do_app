1707cdd42217853a0cb680c737ad3caadd78513e
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-08-28 05:32:16 +0300

    add the head of the second section

--- lib/features/home/presentation/views/widgets/home_screen_body.dart
+++ lib/features/home/presentation/views/widgets/home_screen_body.dart
@@ -25,6 +25,64 @@ class HomeScreenBody extends StatelessWidget {
             ),
             selectionColor: const Color.fromRGBO(6, 69, 83, 1),
           ),
+          const ToDoSeeAllWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class ToDoSeeAllWidget extends StatelessWidget {
+  const ToDoSeeAllWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Text(
+                'To do',
+                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      color: const Color.fromRGBO(6, 69, 83, 1),
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 20.0),
+                height: 36,
+                width: 24,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(255, 173, 71, 1),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Center(
+                  child: Text(
+                    '7',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge!
+                        .copyWith(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'See all',
+              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                    color: Colors.grey,
+                    fontSize: 18,
+                  ),
+            ),
+          ),
         ],
       ),
     );
