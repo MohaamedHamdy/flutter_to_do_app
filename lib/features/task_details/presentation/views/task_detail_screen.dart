d70f14238cc52a3caade8d4c0a7671a5823d5bc9
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-02 21:06:11 +0300

    task detaiil screen app bar done

--- lib/features/task_details/presentation/views/task_detail_screen.dart
+++ lib/features/task_details/presentation/views/task_detail_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:todo/core/utils/colors.dart';
 
 class TaskDetailScreen extends StatelessWidget {
   const TaskDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back_outlined,
+          ),
+        ),
+        title: Text(
+          'Task detail',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Colors.grey,
+                fontSize: 16,
+              ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.penToSquare,
+              size: 24,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
