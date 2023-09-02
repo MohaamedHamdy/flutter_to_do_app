d1bafe47ab609a30a7b1f09d912890bc810a350b
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-02 22:59:07 +0300

    task detail => second section done

--- lib/features/task_details/presentation/views/widgets/task_detail_body.dart
+++ lib/features/task_details/presentation/views/widgets/task_detail_body.dart
@@ -9,7 +9,11 @@ class TaskDetailBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.only(
+        left: 20.0,
+        top: 20.0,
+        bottom: 30.0,
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -23,101 +27,167 @@ class TaskDetailBody extends StatelessWidget {
           const SizedBox(
             height: 30,
           ),
-          Row(
+          const Row(
             children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: Row(
-                  children: [
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(
-                          8,
-                        ),
-                      ),
-                      child: const Center(
-                        child: Icon(
-                          FontAwesomeIcons.snapchat,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Project',
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 6,
-                        ),
-                        const Text(
-                          'SnapChat',
-                          style: TextStyle(
-                            color: MyColors.mainColor,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+              ProjectWidget(),
+              DeadlineWidget(),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 30),
+            child: Text(
+              'Description',
+              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                    color: MyColors.mainColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 20.0),
+            child: Text(
+              'You need to make a Splash screen for the applicaton, Style: minimalism. Use one primary color and one accent color. You can use illustrations, 3D animations and other things to attract attention ',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 30),
+            child: Text(
+              'Members',
+              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                    color: MyColors.mainColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DeadlineWidget extends StatelessWidget {
+  const DeadlineWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: [
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                8,
+              ),
+            ),
+            child: const Center(
+              child: Icon(
+                Icons.calendar_month_rounded,
+                color: MyColors.myYellow,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Deadline',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
                 ),
               ),
-              Row(
-                children: [
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(
-                        8,
-                      ),
+              const SizedBox(
+                height: 6,
+              ),
+              const Text(
+                'Mon, 7 March',
+                style: TextStyle(
+                  color: MyColors.mainColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProjectWidget extends StatelessWidget {
+  const ProjectWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.5,
+      child: Row(
+        children: [
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                8,
+              ),
+            ),
+            child: Center(
+              // child: Icon(
+              //   FontAwesomeIcons.snapchat,
+              // ),
+              child: Container(
+                height: 30,
+                width: 30,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      'https://i.pinimg.com/originals/b0/74/ac/b074acdacc801e9b57930b31d6655fb8.png',
                     ),
-                    child: const Center(
-                      child: Icon(
-                        Icons.calendar_month_rounded,
-                        color: MyColors.myYellow,
-                      ),
-                    ),
                   ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Deadline',
-                        style: TextStyle(
-                          color: Colors.black.withOpacity(0.5),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 6,
-                      ),
-                      const Text(
-                        'Mon, 7 March',
-                        style: TextStyle(
-                          color: MyColors.mainColor,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Project',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              const Text(
+                'SnapChat',
+                style: TextStyle(
+                  color: MyColors.mainColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
             ],
           ),
