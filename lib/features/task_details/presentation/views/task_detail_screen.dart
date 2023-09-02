60c88ebab300b1dc695f1f33d3c4d8a33fea77ce
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-02 21:08:12 +0300

    task detail screen body created

--- lib/features/task_details/presentation/views/task_detail_screen.dart
+++ lib/features/task_details/presentation/views/task_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:todo/core/utils/colors.dart';
+
+import 'widgets/task_detail_body.dart';
 
 class TaskDetailScreen extends StatelessWidget {
   const TaskDetailScreen({super.key});
@@ -33,6 +34,7 @@ class TaskDetailScreen extends StatelessWidget {
           )
         ],
       ),
+      body: TaskDetailBody(),
     );
   }
 }
